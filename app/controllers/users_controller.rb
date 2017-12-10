2ea112e4dd8465458148e9c5458192b0fc67a246
David Maurer <davemaurermail@gmail.com>
2017-12-09 21:27:24 -0700

    add link and view for user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < Clearance::UsersController
+  def show
+    @user = User.find(params[:id])
+    @reaches = @user.reaches
+  end
+
   private
 
   def user_params
--- app/models/reach.rb
+++ app/models/reach.rb
@@ -5,4 +5,6 @@ class Reach < ApplicationRecord
   validates :user, presence: true
 
   default_scope { order(created_at: :desc) }
+
+  delegate :username, to: :user
 end
