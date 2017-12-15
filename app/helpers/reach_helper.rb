3844114630c85fb3bc5980cb8073b3ea9f00a3cd
David Maurer <davemaurermail@gmail.com>
2017-12-15 13:22:41 -0700

    refactor dashboard forms

--- app/helpers/reach_helper.rb
+++ app/helpers/reach_helper.rb
@@ -1,4 +1,12 @@
 module ReachHelper
+  def reach_form_for(content_type)
+    form_for Reach.new do |form|
+      form.hidden_field(:content_type, value: content_type) +
+      form.fields_for(:content) { |content_form| yield(content_form) } +
+      form.submit("Reach Out!")
+    end
+  end
+
   def like_button(reach)
     if current_user.liked?(reach)
       link_to "Unlike", unlike_reach_path(reach), method: :delete
