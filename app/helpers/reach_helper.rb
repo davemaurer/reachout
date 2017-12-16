7697a012d87b14d2362134090bdf30048372e42e
David Maurer <davemaurermail@gmail.com>
2017-12-15 23:43:56 -0700

    remove unnecessary line

--- app/helpers/reach_helper.rb
+++ app/helpers/reach_helper.rb
@@ -1,7 +1,6 @@
 module ReachHelper
   def reach_form_for(content_type)
     form_for Reach.new, url: content_type.new do |form|
-      form.hidden_field(:content_type, value: content_type) +
       form.fields_for(:content) { |content_form| yield(content_form) } +
       form.submit("Reach Out!")
     end
