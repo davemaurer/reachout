affb021e35d5d9272c28f602d3b058edc8821292
David Maurer <davemaurermail@gmail.com>
2017-12-16 18:42:51 -0700

    render reaches with hashtags in hashtag view

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,5 +1,7 @@
 class HashtagsController < ApplicationController
   def show
     @hashtag = params[:id]
+    @results = Reach.joins("LEFT JOIN text_reaches ON content_type = 'TextReach' AND content_id = text_reaches.id").
+     where("text_reaches.body LIKE ?", "%##{@hashtag}%")
   end
 end
