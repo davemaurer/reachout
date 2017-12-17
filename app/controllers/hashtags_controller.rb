5a6d80a7b0a83b52216106128a3256437d154de0
David Maurer <davemaurermail@gmail.com>
2017-12-16 19:51:37 -0700

    add create a search service object to take care of directing database queries

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,6 +1,11 @@
 class HashtagsController < ApplicationController
   def show
-    @hashtag = params[:id]
-    @results = ReachSearchQuery.new(term: "##{@hashtag}").to_relation
+    @search = Search.new(term: hashtag)
+  end
+
+  private
+
+  def hashtag
+    params[:id]
   end
 end
